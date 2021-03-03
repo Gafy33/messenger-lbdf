35bd21e4cbd71b11e25e571a1cc90ff57c79c586
Armand HOUPIN <armando.houpin@hotmail.fr>
2021-03-03 22:43:58 +0100

    modif image + fichier

--- resources/views/vendor/Chatify/layouts/messageCard.blade.php
+++ resources/views/vendor/Chatify/layouts/messageCard.blade.php
@@ -36,20 +36,34 @@
         </div>
         @endif  
     @else
+        @if(@$attachment[2] == 'file')
+        <div class="message-card" data-id="{{ $id }}">
+        <p>
+        <a href="{{ $attachment[0] }}" style="color: #595959;" class="file-download" target="_blank">
+            <span class="fas fa-file"></span> {{$attachment[1]}}</a>
+            <br>
+                    <sub title="{{ $fullTime }}" class="message-time">
+                    <span class="fas fa-{{ $seen > 0 ? 'check-double' : 'check' }} seen"></span> {{ $time }}
+                    </sub>
+        </p>
+        </div>
+        @endif
+        
         @if(@$attachment[2] == 'image')
         <div>
             <div class="message-card" data-id="{{ $id }}">
                 <div class="image-file" style="width: auto; height: auto;">
                     <img class="chat-image" src="{{ $attachment[0] }}" style="max-width: 400px; max-height: 300px;background-image: url('{{ $attachment[0] }}')">
                 </div>
+            </div>
+        </div>
+            <div class="message-card" data-id="{{ $id }}">
                 <p>
-                    <br>
                     <sub title="{{ $fullTime }}" class="message-time">
                     <span class="fas fa-{{ $seen > 0 ? 'check-double' : 'check' }} seen"></span> {{ $time }}
                     </sub>
                 </p>
             </div>
-        </div>
         @endif
     @endif
     @endif
@@ -97,6 +111,10 @@
         <p>
         <a href="{{ $attachment[0] }}" style="color: #595959;" class="file-download" target="_blank">
             <span class="fas fa-file"></span> {{$attachment[1]}}</a>
+            <br>
+                    <sub title="{{ $fullTime }}" class="message-time">
+                    <span class="fas fa-{{ $seen > 0 ? 'check-double' : 'check' }} seen"></span> {{ $time }}
+                    </sub>
         </p>
         </div>
         @endif
@@ -107,14 +125,15 @@
                 <div class="image-file" style="width: auto; height: auto;">
                     <img class="chat-image" src="{{ $attachment[0] }}" style="max-width: 400px; max-height: 300px;background-image: url('{{ $attachment[0] }}')">
                 </div>
+            </div>
+        </div>
+            <div class="message-card mc-sender" data-id="{{ $id }}">
                 <p>
-                    <br>
                     <sub title="{{ $fullTime }}" class="message-time">
                     <span class="fas fa-{{ $seen > 0 ? 'check-double' : 'check' }} seen"></span> {{ $time }}
                     </sub>
                 </p>
             </div>
-        </div>
         @endif
     @endif
     
