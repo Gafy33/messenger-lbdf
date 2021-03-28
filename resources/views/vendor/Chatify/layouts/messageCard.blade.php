9af632f54dabd1517bcb4aaf5c26a0d6484aa1ca
Armand HOUPIN <armando.houpin@hotmail.fr>
2021-03-28 15:00:09 +0200

    pp

--- resources/views/vendor/Chatify/layouts/messageCard.blade.php
+++ resources/views/vendor/Chatify/layouts/messageCard.blade.php
@@ -4,7 +4,7 @@
     @if( nl2br($message) != NULL)
         <div data-id="{{ $id }}">
             <div class="message-card-user" style="align-items: center;">
-                <div class="avatar av-message header-avatar" style="margin: 0px 3px;margin-top: auto;background-image: url('{{ $avatar_from_id }}') ">
+                <div class="avatar av-message header-avatar" style="margin: 0px 3px;margin-top: auto;background-image: url('{{ Auth::user()->avatar }}') ">
                 </div>
                 <span title="{{ $fullTime }}" class="message-time" style="font-size: 10px;"> {{ $time }} </span>
             </div>
@@ -41,7 +41,7 @@
         @if(@$attachment[2] == 'file')
         <div data-id="{{ $id }}">
             <div class="message-card-user" style="align-items: center;">
-                <div class="avatar av-message header-avatar" style="margin: 0px 3px;margin-top: auto;background-image: url('{{ $avatar_from_id }}') ">
+                <div class="avatar av-message header-avatar" style="margin: 0px 3px;margin-top: auto;background-image: url('{{ Auth::user()->avatar }}') ">
                 </div>
                 <span title="{{ $fullTime }}" class="message-time" style="font-size: 10px;"> {{ $time }} </span>
             </div>
@@ -58,7 +58,7 @@
         <div>
             <div data-id="{{ $id }}" style="align-items: center;">
                 <div class="message-card-user" >
-                    <div class="avatar av-message header-avatar" style="margin: 0px 3px;margin-top: auto;background-image: url('{{ $avatar_from_id }}') ">
+                    <div class="avatar av-message header-avatar" style="margin: 0px 3px;margin-top: auto;background-image: url('{{ Auth::user()->avatar }}') ">
                     </div>
                     <span title="{{ $fullTime }}" class="message-time" style="font-size: 10px;"> {{ $time }} </span>
                     
